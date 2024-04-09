ab4642fc83b9a0ba6df758fa35e5ea27b574b506
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-09 20:21:57 -0300

    Conexão ao banco de dados

---
+++ src/instances/mysql.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+export const sequelize = new Sequelize(
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+        dialect: 'mysql',
+        port: parseInt(process.env.MYSQL_PORT)
+    }
+
+)
