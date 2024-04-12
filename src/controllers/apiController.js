812adca9e3dd299c6f94f502b291fc0790dd5f69
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-11 23:04:29 -0300

    Implementando nova funcionalidade no método createTransaction, ao criar uma transação com o payment_method 'debit_card' um payable correspondente também é criado.

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,7 +1,8 @@
+const Payable = require("../models/Payable");
 const Transaction = require("../models/Transaction")
 
 
-// Função para verificar se o numero do cartão é válido e está no formato correto
+// Função para verificar se o numero do cartão é válido e está no formato correto usando o algoritmo de luhn
 const isValidCardNumber = (num) => {
     num = num.replace(/\s/g, '')
     const arr = num
@@ -42,18 +43,26 @@ module.exports = {
                 description,
                 payment_method,
                 card_number,
-                card_name, card_expiration_date,
+                card_name, 
+                card_expiration_date,
                 card_cvv
             } = req.body
 
+            let last4Digits
+
         try {
-            if (!amount || !description || !payment_method || !card_name || !card_expiration_date || !card_cvv) {
+            if (!amount || !description || !payment_method || !card_number || !card_name || !card_expiration_date || !card_cvv) {
                 return res.status(404).json({ error: 'Preencha todos os campos' })
             }
 
-            let last4Digits
-            if (isValidCardNumber(card_number)) {
+            if(card_cvv.toString().length > 3) {
+                return res.status(404).json({error: 'CVV inválido'})
+            }
+
+            if (isValidCardNumber(card_number) || card_number.length > 4 || card_number.length === 16) {
                 last4Digits = card4Digits(card_number)
+            } else {
+                return res.status(404).json({error: 'Numero de cartão inválido'})
             }
 
             let transactionObj = {
@@ -66,7 +75,19 @@ module.exports = {
                 card_cvv
             }
 
-            await Transaction.create(transactionObj)
+            let newTransaction = await Transaction.create(transactionObj)
+
+            if(newTransaction.payment_method === 'debit_card') {
+                let fee = 0.95
+                    await Payable.create({
+                        status: 'paid',
+                        amount: (newTransaction.amount * fee),
+                        payment_date: newTransaction.created_transaction,
+                        transaction_id: newTransaction.id
+                    })
+            }
+
+
             res.status(201).send("Transação criada com sucesso")
 
         } catch (error) {
